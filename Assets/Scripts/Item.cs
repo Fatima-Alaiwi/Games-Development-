a59bca499cc55e283ef4b55690ff5fd55a626103
Zahraa Hubail <202305220@student.polytechnic.bh>
2026-04-10 23:05:25 +0300

    Added Collectable Sound

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -5,14 +5,20 @@ public class Item : MonoBehaviour
     public string itemName;
     public Sprite itemIcon;
 
+    public AudioClip collectSound;
+
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Something entered trigger: " + other.name);
-        
+
         if (other.CompareTag("Player"))
         {
             InventoryManager.instance.AddItem(itemName, itemIcon);
-            Destroy(gameObject); // disappear after pickup
+
+            // 👇 play sound before destroying
+            AudioSource.PlayClipAtPoint(collectSound, transform.position);
+
+            Destroy(gameObject);
         }
     }
 }
