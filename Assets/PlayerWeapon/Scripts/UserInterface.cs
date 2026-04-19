62e614b3bce722b1421a176f8a74c770e2a3150c
RaghadAlesakfi <202302130@student.polytechnic.bh>
2026-04-19 18:08:00 +0300

    added capsule to lucy and trying to do the health

--- Assets/PlayerWeapon/Scripts/UserInterface.cs
+++ Assets/PlayerWeapon/Scripts/UserInterface.cs
@@ -1,3 +1,24 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
+// using TMPro;
+
+// public class UserInterface : MonoBehaviour
+// {
+//     public static UserInterface Singleton;
+
+//     [Header("Ammo")]
+//     public TextMeshProUGUI bulletCount_Text;
+
+//     void Awake()
+//     { UserInterface.Singleton = this; }
+
+//     public void UpdateBulletCounter(int ammoCount, int maxAmmo)
+//     { bulletCount_Text.text = ammoCount + " / " + maxAmmo; }
+// }
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,9 +32,22 @@ public class UserInterface : MonoBehaviour
     [Header("Ammo")]
     public TextMeshProUGUI bulletCount_Text;
 
+    [Header("Health")]
+    public Slider healthSlider;
+
     void Awake()
-    { UserInterface.Singleton = this; }
+    { 
+        UserInterface.Singleton = this; 
+    }
 
     public void UpdateBulletCounter(int ammoCount, int maxAmmo)
-    { bulletCount_Text.text = ammoCount + " / " + maxAmmo; }
+    { 
+        bulletCount_Text.text = ammoCount + " / " + maxAmmo; 
+    }
+
+    public void UpdateHealthBar(int currentHealth, int maxHealth)
+    {
+        if (healthSlider != null)
+            healthSlider.value = (float)currentHealth / (float)maxHealth;
+    }
 }
