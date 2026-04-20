7249e7a2c22edf4c047d5142381c308463d4ebf3
Zahraa Hubail <202305220@student.polytechnic.bh>
2026-04-20 22:01:24 +0300

    Added Audio to the door

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -2,21 +2,30 @@ using UnityEngine;
 
 public class Door : MonoBehaviour, IInteractable
 {
-    // 1. This variable will now show up in the Unity Inspector
     [SerializeField] private string _interactionText = "Press E to Open Door";
-
-    // 2. The interface now pulls the value from that variable
     public string InteractionText => _interactionText;
 
     public bool isInteractable { get; set; } = true;
     public Transform labelAnchor;
     public Transform LabelAnchor => labelAnchor;
 
+    [Header("Sound")]
+    public AudioClip openingDoorClip;
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     public void Interact()
     {
         if (!isInteractable) return;
 
         transform.Rotate(0, 90, 0);
-        isInteractable = false;  
+        isInteractable = false;
+
+        if (audioSource != null && openingDoorClip != null)
+            audioSource.PlayOneShot(openingDoorClip);
     }
 }
