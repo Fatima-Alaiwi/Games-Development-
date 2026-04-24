cb5dc103def0f7d39a17168969ef466c5add341b
Zahraa Hubail <202305220@student.polytechnic.bh>
2026-04-24 03:06:05 +0300

    Fixed Bomb Configirations

--- Assets/Scripts/BombItem.cs
+++ Assets/Scripts/BombItem.cs
@@ -1,27 +1,30 @@
 using UnityEngine;
 
-public class BombItem : MonoBehaviour
+public class BombItem : MonoBehaviour, IInteractable
 {
+    [field: SerializeField]
+    public string InteractionText { get; set; } = "Pick up Bomb";
+    public bool isInteractable { get; set; } = true;
+    public Transform labelAnchor;
+    public Transform LabelAnchor => labelAnchor;
+
     public string itemName = "DungeonBomb";
     public Sprite itemIcon;
     public AudioClip collectSound;
 
-    private void OnTriggerEnter(Collider other)
+    public void Interact()
     {
-        if (!other.CompareTag("Player")) return;
-
         bool added = InventoryManager.instance.AddItem(itemName, itemIcon);
 
         if (added)
         {
-            // Update dragon quest bomb counter
             if (QuestManager.Instance != null)
                 QuestManager.Instance.UpdateQuestCount(itemName, 1);
 
             if (collectSound != null)
                 AudioSource.PlayClipAtPoint(collectSound, transform.position);
 
-            Destroy(gameObject);
+            gameObject.SetActive(false);
         }
         else
         {
