9ad72cf9a2321d5288d26fe0e6e7bf6fe4c6ce2c
mahujairi <manaf.alhujairi2006@gmail.com>
2026-05-28 19:14:39 +0300

    fixed gun text

--- Assets/Scripts/Lab-Level/PickUpGun.cs
+++ Assets/Scripts/Lab-Level/PickUpGun.cs
@@ -7,6 +7,7 @@ public class PickUpGun : MonoBehaviour, IInteractable
 
     [Header("IInteractable Settings")]
     [SerializeField] private string _interactionText = "Press E to Pick Up Gun";
+    [SerializeField] private string _blockedInteractionText = "Not now";
     [SerializeField] private bool _isInteractable = true;
     [SerializeField] private Transform _labelAnchor;
 
@@ -14,23 +15,27 @@ public class PickUpGun : MonoBehaviour, IInteractable
     public GameObject visualModel;
 
     // Interface Properties
-    public string InteractionText => _interactionText;
+    public string InteractionText => IsCurrentObjective() ? _interactionText : _blockedInteractionText;
     public bool isInteractable { get => _isInteractable; set => _isInteractable = value; }
     public Transform LabelAnchor => _labelAnchor;
 
     public void Interact()
     {
-        if (QuestManager.Instance != null && QuestManager.Instance.activeQuests.Count > 0)
+        if (IsCurrentObjective())
         {
-            if (QuestManager.Instance.activeQuests[0] == gunQuest)
-            {
-                CompleteTheQuest();
-            }
-            else
-            {
-                Debug.Log("<color=yellow>INTERACTION BLOCKED:</color> This isn't your current objective.");
-            }
+            CompleteTheQuest();
         }
+        else
+        {
+            Debug.Log("<color=yellow>INTERACTION BLOCKED:</color> This isn't your current objective.");
+        }
+    }
+
+    private bool IsCurrentObjective()
+    {
+        return QuestManager.Instance != null &&
+               QuestManager.Instance.activeQuests.Count > 0 &&
+               QuestManager.Instance.activeQuests[0] == gunQuest;
     }
 
     private void CompleteTheQuest()
